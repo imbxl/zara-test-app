b79f8eb5183d8a4b0313e744f0f1cb35ba8fb420
imbxl <imbxl@hotmail.com>
2022-12-15 13:11:26 +0100

    Rutas
    - Creación de Rutas (Home, Podcast y Episode)
    - Organización de archivos

---
+++ src/episode.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Episode extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Podcaster</h1>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Home from './home';
+import Podcast from './podcast';
+import Episode from './episode';
+import Error404 from './404';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/podcast/:podcastId" element={<Podcast />} />
+          <Route path="/podcast/:podcastId/episode/:episodeId" element={<Episode />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </Router>
   </React.StrictMode>
 );
